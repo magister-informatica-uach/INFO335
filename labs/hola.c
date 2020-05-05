acc989109bc1c5b04e737d40bfd9a89f73964b0b
cristobal <cristobal.navarro.g@gmail.com>
2020-05-05 19:25:48 -0400

    benchmark updates

---
+++ labs/hola.c
@@ -0,0 +1,43 @@
+int main(){
+    int c = func();
+    int d = x*y + log(x);
+    int q = x*y + log(x);
+    A[2] = B[c];
+    A[3] = B[c];
+    // CONDICIONAL -> RAMIFICACION EN EL CODIGO
+    // SE ARRIESGA CON UNA RAMA DE EJECUCION
+    if( c > 0 ){
+        //
+        //...
+        //
+    else{
+        //
+        //..
+    }
+    //...
+    //
+    //
+    //
+    // INSTRUCCION CONTEXTO A (Mas de aritmetica)
+    // INSTRUCCION CONTEXTO B (Mas de I/O, o acceso a memoria)
+    // INSTRUCCION CONTEXTO A (Mas de aritmetica)
+    // INSTRUCCION CONTEXTO B (Mas de I/O, o acceso a memoria)
+    // INSTRUCCION CONTEXTO A (Mas de aritmetica)
+    // INSTRUCCION CONTEXTO B (Mas de I/O, o acceso a memoria)
+    //
+    //
+    // INSTRUCCION CONTEXTO A (Mas de aritmetica)   [1 unidad de flotante]
+    // INSTRUCCION CONTEXTO A (Mas de aritmetica)
+    // INSTRUCCION CONTEXTO A (Mas de aritmetica)
+    // INSTRUCCION CONTEXTO A (Mas de aritmetica)
+    // INSTRUCCION CONTEXTO A (Mas de aritmetica)
+    // INSTRUCCION CONTEXTO A (Mas de aritmetica)
+    // INSTRUCCION CONTEXTO A (Mas de aritmetica)
+    // INSTRUCCION CONTEXTO B (Mas de I/O, o acceso a memoria)
+    // INSTRUCCION CONTEXTO B (Mas de I/O, o acceso a memoria)
+    // INSTRUCCION CONTEXTO B (Mas de I/O, o acceso a memoria)
+    // INSTRUCCION CONTEXTO B (Mas de I/O, o acceso a memoria)
+    // INSTRUCCION CONTEXTO B (Mas de I/O, o acceso a memoria)
+    // INSTRUCCION CONTEXTO B (Mas de I/O, o acceso a memoria)
+    // INSTRUCCION CONTEXTO B (Mas de I/O, o acceso a memoria)
+}
