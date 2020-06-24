b56543ea722f1fdc137baabad0ae515447e688fb
cristobal <cristobal.navarro.g@gmail.com>
2020-06-23 20:34:00 -0400

    fixed varios

--- labs/lab05-pointer-jumping/main.cpp
+++ labs/lab05-pointer-jumping/main.cpp
@@ -3,19 +3,25 @@
 #include <omp.h>
 
 void printarray(int *a, int n, const char *msg){
-	printf("%s:\n", msg);
-	for(int i=0; i<n; ++i){
-		printf("%i  ", i);
-	}
-	printf("\n");
-	for(int i=0; i<n; ++i){
-		printf("%i  ", a[i]);
-	}
-	printf("\n");
-	printf("\n");
+    if( n <= 32) {
+        printf("%s:\n", msg);
+        for(int i=0; i<n; ++i){
+            printf("%i  ", i);
+        }
+        printf("\n");
+        for(int i=0; i<n; ++i){
+            printf("%i  ", a[i]);
+        }
+        printf("\n");
+        printf("\n");
+    }
 }
 
+// analisis problema: subir la cota minima lo mas posible (Omega(f(n)))
+// analisis algoritmo: bajar la cota superior de tiempo (O(f(n))
+// pointer jumping (algoritmo)
 void pointer_jumping(int *p, int n){
+
 }
 
 
