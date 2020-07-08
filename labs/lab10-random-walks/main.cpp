a9ba28ddc0cb890b1f26b8d9114b5ef15b468013
cristobal <cristobal.navarro.g@gmail.com>
2020-07-07 20:06:17 -0400

    lab wandom walk updated

--- labs/lab10-random-walks/main.cpp
+++ labs/lab10-random-walks/main.cpp
@@ -2,9 +2,45 @@
 #include <omp.h>
 #include <vector>
 #include <random>
+#include <ctime>
+#include "tools.h"
 using namespace std;
 
 int main(int argc, char **argv){
-    // programacion
+    // (1) argumentos  ./prog n k m
+    if(argc != 4){
+        fprintf(stderr, "run as ./prog n k m\n");
+        exit(EXIT_FAILURE);
+    }
+    int n = atoi(argv[1]);
+    int k = atoi(argv[2]);
+    int m = atoi(argv[3]);
+    // (2) crear e iniciar dominio n x n
+    int *mat = new int[n * n];
+    init_mat_const(mat, n, 0);
+
+    // (3) ubicar lobito cachorro 
+    srand(time(NULL));
+    ubicar_lobito(mat, n);
+
+    // (4) ubicar lobito cachorro y manada
+    vector<pair<int, int>> manada(k);
+    if(m == 0){
+        ubicar_manada_det(mat, n, manada);
+    }
+    else if(m == 1){
+        ubicar_manada_nondet(mat, n, manada);
+    }
+    print_mat(mat, n);
+    getchar();
+
+    // (5) proceso de busqueda
+    if(m ==0){
+        //pair<int,int> p = busqueda_det(mat, n, manada);
+    }
+    else if(m == 1){
+        pair<int,int> p = busqueda_nodet(mat, n, manada);
+    }
+    return EXIT_SUCCESS;
 }
 
--- labs/lab10-random-walks/tools.h
+++ labs/lab10-random-walks/tools.h
@@ -1,6 +1,7 @@
 #ifndef TOOLS_H
 #define TOOLS_H
-
+using namespace std;
+const char sym[3] = {'*', '@', 'L'};
 void init_mat_const(int *mat, int n, int c){
     #pragma omp parallel for
     for(int i=0; i<n; ++i){
@@ -15,35 +16,70 @@ void print_mat(int *mat, int n){
     for(int i=0; i<n; ++i){
         for(int j=0; j<n; ++j){
             long index = i*n + j;
-            if(mat[index] != 0){
-                printf("%i ", mat[index]);
-            }
-            else{
-                printf("* ");
-            }
+            printf("%c ", sym[mat[index]]);
         }
         printf("\n");
     }
 }
-
-void print_mat(int *mat, int n, std::vector<std::pair<int,int>> b){
-    for(int i=0; i<b.size(); ++i){
-        long index = b[i].first * n + b[i].second;
-        mat[index] = 9;
-    }
-    print_mat(mat, n);
-    for(int i=0; i<b.size(); ++i){
-        long index = b[i].first * n + b[i].second;
-        mat[index] = 0;
-    }
+// ubicar cachorro
+void ubicar_lobito(int *mat, int n){
+    int i = rand() % n;
+    int j = rand() % n;
+    mat[i*n + j] = 1;
 }
 
+void ubicar_manada_det(int *mat, int n, vector<pair<int,int>> &man){
+    // dividir el dominio para k lobos
+    // considerar particion 2D, factorizar k en dos valores 'similares'.
+     
+}
+
+void ubicar_manada_nondet(int *mat, int n, vector<pair<int,int>> &man){
+    // pintar en la matriz
+    if(man.size() > 0){
+        // todos parten de un mismo lugar
+        int i = rand() % n;
+        int j = rand() % n;
+        while(mat[i*n + j] == 1){
+            i = rand() % n;
+            j = rand() % n;
+        }
+        mat[i*n + j] = 2;
+        // inicializar la manada con (i,j)
+        for(int k=0; k<man.size(); ++k){
+            // cada lobo parte en distinto lugar
+            //int i = rand() % n;
+            //int j = rand() % n;
+            //while(mat[i*n + j] == 1){
+            //    i = rand() % n;
+            //    j = rand() % n;
+            //}
+            //mat[i*n + j] = 2;
+            man[k] = {i,j};
+        }
+    }
+}
 
 // busqueda deterministica 
-std::pair<int, int> search_det(int *mat, int n, int g){
-}
-
+std::pair<int, int> busqueda_det(int *mat, int n, vector<pair<int,int>> &m){}
 // busqueda no-deterministica
-std::pair<int, int> search_nondet(int *mat, int n, int g){
+std::pair<int, int> busqueda_nodet(int *mat, int n, vector<pair<int,int>> &m){
+    int perdido = 1;
+    // while (no encontrado)
+    while(perdido){
+        // un paso de busqueda
+        for(int k=0; k<m.size(); ++k){
+            mat[m[k].first*n + m[k].second] = 0;
+            int i = (m[k].first + ((rand() % 3) - 1)) % n;
+            int j = (m[k].second + ((rand() % 3) - 1)) % n;
+            m[k] = {i,j};
+            mat[m[k].first*n + m[k].second] = 2;
+        }
+        //#ifdef DEBUG
+            print_mat(mat, n);
+            getchar();
+        //#endif
+    }
+    return {0,0};
 }
 #endif
